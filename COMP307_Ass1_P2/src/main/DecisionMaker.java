b9e5cbe7a9dd78cd699f24fc5b36d9bd46c3cd43
CameronProbert <c.probert@hotmail.co.nz>
2015-03-28 11:40:19 +1300

    Program now runs, no functionality except the base code given though

--- COMP307_Ass1_P2/src/main/DecisionMaker.java
+++ COMP307_Ass1_P2/src/main/DecisionMaker.java
@@ -13,8 +13,64 @@ public class DecisionMaker {
 	List<String> attNames;
 	List<Instance> allInstances;
 	
+	String fileName1;
+	String fileName2;
 	
-
+	public DecisionMaker(String file1, String file2) {
+		fileName1 = file1;
+		fileName2 = file2;
+		readDataFile(file1);
+		buildTree(allInstances, attNames);
+	}
+	
+	private Instance buildTree(List<Instance> instances, List<String> attributes) {
+	//instances: the set of training instances that have been classified to the node being constructed;
+	//attributes:: the list of attributes that were not used on the path from the root to this node.
+	//BuildTree (Set instances, List attributes)
+			//if instances is empty
+				//return a leaf node containing the name and probability of the overall most probable class (ie, the ‘‘baseline’’ predictor)
+			//if instances are pure
+				//return a leaf node containing the name of the class of the instances in the node and probability 1
+			//if attributes is empty
+				//return a leaf node containing the name and probability of the majority class of the instances in the node (choose randomly if classes are equal)
+			//else find best attribute:
+				//for each attribute
+					//separate instances into two sets: 
+								//instances for which the attribute is true, and
+								//instances for which the attribute is false
+					//compute purity of each set.
+					//if weighted average purity of these sets is best so far
+						//bestAtt = this attribute
+						//bestInstsTrue = set of true instances
+						//bestInstsFalse = set of false instances
+				//build subtrees using the remaining attributes:
+					//left = BuildTree(bestInstsTrue, attributes - bestAtt)
+					//right = BuildTree(bestInstsFalse, attributes - bestAttr)
+				//return Node containing (bestAtt, left, right)
+		return null;
+	}
+	
+	/**
+	 * Checks to make sure it is given only 2 filenames and then creates a DecisionMaker
+	 * @param args
+	 */
+	public static void main(String[] args){
+		// First make sure that the arguments are valid
+		try {
+			if (args.length != 2) {
+				throw new Exception(
+						"This program must be given  2 filenames as parameters");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		new DecisionMaker(args[0], args[1]);
+	}
+	
+	/*
+	 * This method given as helper code
+	 */
 	private void readDataFile(String fname) {
 		/*
 		 * format of names file: names of categories, separated by spaces names
@@ -44,6 +100,9 @@ public class DecisionMaker {
 		}
 	}
 
+	/*
+	 * This method given as helper code
+	 */
 	private List<Instance> readInstances(Scanner din) {
 		/* instance = classname and space separated attribute values */
 		List<Instance> instances = new ArrayList<Instance>();
@@ -57,6 +116,9 @@ public class DecisionMaker {
 		return instances;
 	}
 
+	/*
+	 * This class given as helper code
+	 */
 	private class Instance {
 
 		private int category;
