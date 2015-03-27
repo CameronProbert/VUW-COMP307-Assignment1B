0ad7038ccd2841daaf38ad8b5a133939725931a6
CameronProbert <c.probert@hotmail.co.nz>
2015-03-28 12:29:48 +1300

    Added a basic leaf node

---
+++ COMP307_Ass1_P2/src/main/LeafNode.java
@@ -0,0 +1,12 @@
+package main;
+
+public class LeafNode {
+
+	private int classification;
+	private double probability;
+	
+	public LeafNode(int classification, double probability){
+		this.classification = classification;
+		this.probability = probability;
+	}
+}
